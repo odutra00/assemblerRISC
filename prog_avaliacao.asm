fd8b0ac22b52d5cadcbc3efacb0d21c06b8dafa9
odilon <odutra@unifei.edu.br>
2024-12-06 18:38:07 -0300

    Modified prog_avaliacao.asm to give one clock to lw referring to r31.

--- prog_avaliacao.asm
+++ prog_avaliacao.asm
@@ -10,20 +10,20 @@ Loop: 	lw $r1, grupo*0x0250($r31)              ;		r1 ← Mem[i + Número do Grup
 		sw $r10, grupo*0x0250+1023($r15)        ;		MemDados[ultima posição RAM] ←  [r10]
 												;
 												;		Code solves data hazzard by adding bubbles
-		ori $r10, 0($r0)                        ;		r10 ← 0	
-		ori $r31, 0($r0)                        ;		r31	← 0	(auxiliary counter to sweep vector)	
+		ori $r31, 0($r0)                        ;		r10 ← 0	
+		ori $r10, 0($r0)                        ;		r31	← 0	(auxiliary counter to sweep vector)	
         ori $r13, 0x00FF($r0)                   ;       r13 ← 0x00FF (constant to multiply vector sum)
 Loop1: 	lw $r1, grupo*0x0250($r31)              ;       r1 ← Mem[i + Número do Grupo * 0250h] 
 		addi $r31, 1($r31)                      ;		i = i + 1  
 		lw $r17, grupo*0x0250($r31)             ;		lw working as nop for bubble
 		lw $r17, grupo*0x0250($r31)             ;		lw working as nop for bubble
 		add $r10, $r1, $r10                     ;		r10 ← r1 + r10 (cumulative sum of nth vector value)
-		lw $r17, grupo*0x0250($r31)             ;		lw working as nop for bubble
-		lw $r17, grupo*0x0250($r31)             ;		lw working as nop for bubble
+                                                ;		NOP as bubble
+		                                        ;		NOP as bubble
 		bne $r31, $r30, Loop1                   ;		if r31 != r30 then PC ← Loop1 Address
         mul $r20, $r10, $r13                    ;       r20 ← sum * 0x00FF
-                                                ;
-                                                ;
+                                                ;       NOP as bubble
+                                                ;       NOP as bubble
 		sw $r20, grupo*0x0250+1023($r15)        ;		MemDados[ultima posição RAM] ←  [r10]
 		                                        ;
 		jmp Main                                ;		PC ← Main Address
